e7b8c768fae84c5e9614e4203f8733478f22ba96
Allison Hamburger <hamburge@adobe.com>
2015-06-18 14:30:15 -0700

    Backspace to clear the search bar filter; some style changes

--- src/js/jsx/search/SearchBar.jsx
+++ src/js/jsx/search/SearchBar.jsx
@@ -176,7 +176,7 @@ define(function (require, exports, module) {
             // Get list of layers
             var appStore = this.getFlux().store("application"),
                 document = appStore.getCurrentDocument(),
-                layers = document.layers.allVisible,
+                layers = document.layers.allVisible.reverse(),
                 layerMap = layers.map(function (layer) {
                     // Used to determine the layer face icon
                     var iconID = this._getSVGInfo(layer),
@@ -373,6 +373,18 @@ define(function (require, exports, module) {
             }.bind(this));
         },
 
+        _handleKeyDown: function (event) {
+            switch (event.key) {
+                case "Backspace": {
+                    if (!this.refs.dataList.hasNonEmptyInput()) {
+                        this.setState({
+                            filter: ""
+                        });
+                    }
+                }
+            }
+        },
+
         render: function () {
             var searchOptions = this._getAllSelectOptions();
 
@@ -384,6 +396,7 @@ define(function (require, exports, module) {
                         {this.state.filter}
                     </div>
                    <Datalist
+                    ref="dataList"
                     live={false}
                     className="dialog-search-bar"
                     options={searchOptions}
@@ -392,6 +405,7 @@ define(function (require, exports, module) {
                     placeholderText="Type to search"
                     filter={this._filterSearch}
                     onChange={this._handleChange}
+                    onKeyDown={this._handleKeyDown}
                     />
                 </div>
             );
